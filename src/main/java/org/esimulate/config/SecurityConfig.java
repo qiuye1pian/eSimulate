14f47a5397b2c963b23addb8ce083f87b2b2d674
chenhonghe <qiuye1pian@gmail.com>
2025-03-09 15:41:42 +0100

    暂时放开所有接口

--- src/main/java/org/esimulate/config/SecurityConfig.java
+++ src/main/java/org/esimulate/config/SecurityConfig.java
@@ -24,17 +24,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                // 关闭 CSRF 保护（针对 API）
-//                .csrf().disable().authorizeRequests()
-//                // 允许公开访问
-//                .antMatchers("/api/login", "/api/register","/api/thermal-load-schemes/test").permitAll()
-//                // 其他请求需要认证
-//                .anyRequest().authenticated().and()
-//                // 禁用 Session 认证（如果只用 JWT）
-//                .sessionManagement().disable()
-//                // ❌ 禁用默认的 Spring Security 登录页面
-//                .formLogin().disable();
+        /*
+          http
+                        // 关闭 CSRF 保护（针对 API）
+                        .csrf().disable().authorizeRequests()
+                        // 允许公开访问
+                        .antMatchers("/api/login", "/api/register","/api/thermal-load-schemes/test").permitAll()
+                        // 其他请求需要认证
+                        .anyRequest().authenticated().and()
+                        // 禁用 Session 认证（如果只用 JWT）
+                        .sessionManagement().disable()
+                        // ❌ 禁用默认的 Spring Security 登录页面
+                        .formLogin().disable();
+         */
         http
                 .csrf().disable()  // 关闭 CSRF 保护（针对 API）
                 .authorizeRequests()
