8972aee58bf6241a1772b8a66c8233a6ca729b1f
chenhonghe <qiuye1pian@gmail.com>
2025-03-09 14:22:41 +0100

    改版本

--- src/main/java/org/esimulate/util/JwtUtil.java
+++ src/main/java/org/esimulate/util/JwtUtil.java
@@ -1,17 +1,17 @@
 package org.esimulate.util;
 
 import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.SignatureException;
 import org.springframework.stereotype.Component;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private String secret = "mySecretKey"; // 你可以使用环境变量存储密钥
-    private long expirationMs = 86400000; // 1 天
+    private final String secret = "mySecretKey"; // 你可以使用环境变量存储密钥
 
     public String generateToken(String username) {
+        // 1 天
+        long expirationMs = 86400000;
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
