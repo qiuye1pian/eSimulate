73850d4e1ec8ec449d5fd53ca5e64a18cc9d0a2b
chenhonghe <qiuye1pian@gmail.com>
2025-03-23 19:35:48 +0100

    改成无年份的值

--- src/main/java/org/esimulate/util/DateTimeUtil.java
+++ src/main/java/org/esimulate/util/DateTimeUtil.java
@@ -9,7 +9,7 @@ public class DateTimeUtil {
 
     public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
 
-    public static final String MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+    public static final String MM_DD_HH_MM_SS = "MM-dd HH:mm:ss";
 
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(YYYY_MM_DD_HH_MM_SS);
 
