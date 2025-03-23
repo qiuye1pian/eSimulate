18ebef5bd87b09bf8d2f08a0992d891e88a5c908
chenhonghe <qiuye1pian@gmail.com>
2025-03-23 17:57:58 +0100

    加个JSON测试

--- src/test/java/org/esimulate/core/pojo/WindPowerChartDtoTest.java
+++ src/test/java/org/esimulate/core/pojo/WindPowerChartDtoTest.java
@@ -1,6 +1,7 @@
 package org.esimulate.core.pojo;
 
 
+import com.alibaba.fastjson2.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -116,4 +117,25 @@ class WindPowerChartDtoTest {
                 .intValue();
         Assertions.assertEquals(new BigDecimal(expectedMaxInt), dto.getYAxis().getMax(), "yAxis max 计算不正确");
     }
+
+    @Test
+    void testToJson() {
+        // 构造测试数据
+        List<BigDecimal> xAxisData = Arrays.asList(
+                new BigDecimal("1.0"),
+                new BigDecimal("2.0"),
+                new BigDecimal("3.0"));
+        List<BigDecimal> seriesData = Arrays.asList(
+                new BigDecimal("5.0"),
+                new BigDecimal("10.0"),
+                new BigDecimal("7.5"));
+        // 当 seriesData 中最大值为 10，该值乘以 1.33 后为 13.3，设置 scale 0 且舍去小数点部分最后应该为 13
+        WindPowerChartDto dto = new WindPowerChartDto(xAxisData, seriesData);
+        // 将对象转换为 JSON 字符串
+        String jsonString = JSONObject.toJSONString(dto);
+        // 断言 JSON 字符串是否符合预期
+        System.out.println(jsonString);
+        Assertions.assertEquals("{\"XAxis\":{\"axisLabel\":{\"formatter\":\"{value} m/s\"},\"boundaryGap\":false,\"data\":[1.0,2.0,3.0],\"type\":\"category\"},\"YAxis\":{\"max\":13},\"series\":[{\"data\":[5.0,10.0,7.5],\"name\":\"\",\"smooth\":true,\"type\":\"line\"}]}", jsonString);
+    }
+
 }
