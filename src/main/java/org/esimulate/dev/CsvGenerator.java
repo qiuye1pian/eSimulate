1220f84792b40cc020cd3743f901d6d47f86a0a7
chenhonghe <qiuye1pian@gmail.com>
2025-03-26 16:44:59 +0100

    曲线生成器改动一下

--- src/main/java/org/esimulate/dev/CsvGenerator.java
+++ src/main/java/org/esimulate/dev/CsvGenerator.java
@@ -13,9 +13,16 @@ public class CsvGenerator {
 
     /**
      * 曲线生成器
+     *
      * @param args 没有参数
      */
     public static void main(String[] args) {
+
+        String init = "21.00";
+        String jump = "1.00";
+        String valueTitle = "温度";
+        String min = "-30.00";
+        String max = "45.99";
 
         // 设置初始时间
         LocalDateTime startTime = LocalDateTime.of(2025, 1, 1, 0, 0);
@@ -24,13 +31,13 @@ public class CsvGenerator {
         // 设置随机数生成器
         Random random = new Random();
         // 设置初始值
-        BigDecimal previousValue = new BigDecimal("5000000.00");
+        BigDecimal previousValue = new BigDecimal(init);
         // 设置波动范围
-        BigDecimal maxFluctuation = new BigDecimal("50000.00");
+        BigDecimal maxFluctuation = new BigDecimal(jump);
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/chenhonghe/Desktop/华北电力大学/县域多能互补一体化平台/脚本/上传模板/load_values.csv"))) {
             // 写入 CSV 表头
-            writer.write("时间,负荷值\n");
+            writer.write(String.format("%s,%s\n", "时间", valueTitle));
 
             // 写入每一行数据
             for (int i = 0; i < 365 * 24; i++) { // 一年的小时数
@@ -41,10 +48,12 @@ public class CsvGenerator {
                 // 计算当前负荷值
                 BigDecimal currentValue = previousValue.add(fluctuation);
                 // 确保负荷值在指定范围内
-                if (currentValue.compareTo(new BigDecimal("10000.00")) < 0) {
-                    currentValue = new BigDecimal("10000.00");
-                } else if (currentValue.compareTo(new BigDecimal("9999999.99")) > 0) {
-                    currentValue = new BigDecimal("9999999.99");
+                if (currentValue.compareTo(new BigDecimal(min)) < 0) {
+                    currentValue = new BigDecimal(min);
+                }
+
+                if (currentValue.compareTo(new BigDecimal(max)) > 0) {
+                    currentValue = new BigDecimal(max);
                 }
                 // 写入当前行
                 writer.write(String.format("%s,%.2f\n", currentTime.format(formatter), currentValue.setScale(2, RoundingMode.HALF_UP)));
