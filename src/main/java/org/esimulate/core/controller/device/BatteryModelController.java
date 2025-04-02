6db21767ea14740a3fec5df2f8a2b77d1a8a9b2d
chenhonghe <qiuye1pian@gmail.com>
2025-04-02 15:22:23 +0200

    去掉没用的代码

--- src/main/java/org/esimulate/core/controller/device/BatteryModelController.java
+++ src/main/java/org/esimulate/core/controller/device/BatteryModelController.java
@@ -2,10 +2,8 @@ package org.esimulate.core.controller.device;
 
 import lombok.extern.log4j.Log4j2;
 import org.esimulate.core.model.device.BatteryModel;
-import org.esimulate.core.model.device.HydroPowerPlantModel;
 import org.esimulate.core.pojo.model.BatteryModelDto;
 import org.esimulate.core.pojo.model.BatteryPageQuery;
-import org.esimulate.core.pojo.model.HydroPowerPlantModelDto;
 import org.esimulate.core.service.device.BatteryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,8 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/model/battery")
@@ -25,7 +21,7 @@ public class BatteryModelController {
     private BatteryService batteryService;
 
     @PostMapping("/getListByPage")
-    public Page<BatteryModel> findListByPage(@RequestBody BatteryPageQuery pageQuery) {
+    public Page<BatteryModelDto> findListByPage(@RequestBody BatteryPageQuery pageQuery) {
         return batteryService.findListByPage(pageQuery).map(BatteryModelDto::new);
     }
 
@@ -54,27 +50,6 @@ public class BatteryModelController {
     public String deleteBatteryModel(@RequestBody BatteryModelDto batteryModelDto) {
         batteryService.deleteById(batteryModelDto.getId());
         return "电池模型删除成功";
-    }
-
-    @PostMapping("/calculate_eta")
-    public BigDecimal calculate_eta(@RequestBody HydroPowerPlantModelDto hydroPowerPlantModelDto) {
-        hydroPowerPlantModelDto.setZ1(BigDecimal.ONE);
-        hydroPowerPlantModelDto.setZ2(BigDecimal.ONE);
-        hydroPowerPlantModelDto.setV1(BigDecimal.ONE);
-        hydroPowerPlantModelDto.setV2(BigDecimal.ONE);
-        hydroPowerPlantModelDto.setP1(BigDecimal.ONE);
-        hydroPowerPlantModelDto.setP2(BigDecimal.ONE);
-        hydroPowerPlantModelDto.setPg(BigDecimal.ONE);
-        hydroPowerPlantModelDto.setG(BigDecimal.ONE);
-        return new HydroPowerPlantModel(hydroPowerPlantModelDto).getEta();
-    }
-
-    @PostMapping("/calculate_head")
-    public BigDecimal calculate_head(@RequestBody HydroPowerPlantModelDto hydroPowerPlantModelDto) {
-        hydroPowerPlantModelDto.setEta1(BigDecimal.ONE);
-        hydroPowerPlantModelDto.setEta2(BigDecimal.ONE);
-        hydroPowerPlantModelDto.setEta3(BigDecimal.ONE);
-        return new HydroPowerPlantModel(hydroPowerPlantModelDto).getHead();
     }
 
 }
