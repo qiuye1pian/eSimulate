e681761dd2664f59c0a2246e6a7a684237f375f7
chenhonghe <qiuye1pian@gmail.com>
2025-05-25 17:26:27 +0200

    去掉路径里的model

--- src/main/java/org/esimulate/core/controller/device/CogenerationModelController.java
+++ src/main/java/org/esimulate/core/controller/device/CogenerationModelController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/model/cogeneration-model")
+@RequestMapping("/model/cogeneration")
 @Log4j2
 public class CogenerationModelController {
 
