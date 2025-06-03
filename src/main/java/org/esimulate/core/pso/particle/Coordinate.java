8bd48c979a580212dd9e570afd55b0c692070ea0
chenhonghe <qiuye1pian@gmail.com>
2025-06-03 22:29:08 +0200

    修边功能需要调整

--- src/main/java/org/esimulate/core/pso/particle/Coordinate.java
+++ src/main/java/org/esimulate/core/pso/particle/Coordinate.java
@@ -31,15 +31,6 @@ public class Coordinate implements Dimension, Cloneable {
         this.value = lowerBound + random.nextInt(upperBound) / 2;
     }
 
-    public void setValue(Integer newValue) {
-        if (newValue < lowerBound) {
-            newValue = lowerBound;
-        }
-        if (newValue > upperBound) {
-            newValue = upperBound;
-        }
-        this.value = newValue;
-    }
     /**
      * 克隆
      *
