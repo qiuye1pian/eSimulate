5c3d45e9b1ffd48d70991c12d0abbd583d7a4c5a
chenhonghe <qiuye1pian@gmail.com>
2025-06-15 12:06:54 +0200

    改变返回值

--- src/main/java/org/esimulate/core/pojo/pso/OptimizeResultDto.java
+++ src/main/java/org/esimulate/core/pojo/pso/OptimizeResultDto.java
@@ -11,14 +11,11 @@ import java.util.List;
 @NoArgsConstructor
 @Data
 public class OptimizeResultDto {
-//    private Position globalBestPosition;
-//    private BigDecimal globalBestFitness;
+
     private List<String> positionTitle = new ArrayList<>();
     private List<TaskDetail> taskDetailList = new ArrayList<>();
 
     public OptimizeResultDto(OptimizeTask optimizeTask) {
-//        this.globalBestPosition = optimizeTask.getGlobalBestPosition();
-//        this.globalBestFitness = optimizeTask.getGlobalBestValue();
         this.positionTitle = optimizeTask.getPositionTitle();
         this.taskDetailList = optimizeTask.getTaskDetailList();
     }
