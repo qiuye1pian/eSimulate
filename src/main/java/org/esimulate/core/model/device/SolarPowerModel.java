78bf0a621a824a2ba1e33c18de922e72f11b0bc0
chenhonghe <qiuye1pian@gmail.com>
2025-06-19 09:40:20 +0200

    调整太阳能的输出

--- src/main/java/org/esimulate/core/model/device/SolarPowerModel.java
+++ src/main/java/org/esimulate/core/model/device/SolarPowerModel.java
@@ -122,24 +122,30 @@ public class SolarPowerModel extends Device implements Producer, Dimension, Elec
                 .multiply(this.quantity)
                 .setScale(10, RoundingMode.HALF_UP);
 
-        // 分段平滑：
-        BigDecimal cap = P_pvN.multiply(BigDecimal.valueOf(1.165));
-
         if (outputPower.compareTo(BigDecimal.ZERO) <= 0) {
             outputPower = BigDecimal.ZERO;
             return new ElectricEnergy(outputPower);
         }
 
+        // 分段平滑：
+        BigDecimal cap = P_pvN.multiply(BigDecimal.valueOf(1.165));
+
         if (outputPower.compareTo(cap) <= 0) {
-            // 在 [P, 1.165P] 区间，缓慢增长：系数0.22
+            // 在 [P, 1.165P] 区间，缓慢增长：系数0.09
             BigDecimal delta = outputPower.subtract(P_pvN);
-            outputPower = P_pvN.add(delta.multiply(BigDecimal.valueOf(0.22)))
+            outputPower = P_pvN.add(delta.multiply(BigDecimal.valueOf(0.09)))
                     .setScale(10, RoundingMode.HALF_UP);
         } else {
-            // 超过1.165P后，再次缓慢增长：系数0.1
+            // 超过1.165P后，再次缓慢增长：系数0.01
             BigDecimal delta = outputPower.subtract(cap);
-            outputPower = cap.add(delta.multiply(BigDecimal.valueOf(0.1)))
+            outputPower = cap.add(delta.multiply(BigDecimal.valueOf(0.01)))
                     .setScale(10, RoundingMode.HALF_UP);
+        }
+
+        BigDecimal top = P_pvN.multiply(BigDecimal.valueOf(1.2));
+
+        if (outputPower.compareTo(top) >= 0) {
+            outputPower = top;
         }
 
         return new ElectricEnergy(outputPower);
